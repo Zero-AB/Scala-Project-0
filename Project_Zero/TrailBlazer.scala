7e9dfcc58787af9a21c4f4425ea0ed5b3f053c53
Zero-AB <abell2021@gmail.com>
2022-01-24 17:54:00 -0500

    Update TrailBlazer.scala
    second commit

--- Project_Zero/TrailBlazer.scala
+++ Project_Zero/TrailBlazer.scala
@@ -17,14 +17,18 @@ object RunGame {
 
   def main(args: Array[String]): Unit = {
     // body of main goes here for run time
-    startupFunctions.titlescreen()
+
+   // startupFunctions.runStartUp()
+
     //below is for testing connection
 
-      //Connect_to_DB.connect()
+     // Connect_to_DB.connect()
 
     //below is for checking database being used is correct
 
-      //Connect_to_DB.show_Databases()
+    //  Connect_to_DB.show_Databases()
+
+    Connect_to_DB.show_Usernames()
 
     //sys.exit(0)
   }
@@ -33,6 +37,13 @@ object RunGame {
 
 //create function definitions for startup
 object startupFunctions {
+
+  def runStartUp(): Unit = {
+    startupFunctions.titlescreen()
+    startupFunctions.titleScreenInputs()
+  }
+
+  //display title screen and list of options
   def titlescreen(): Unit = {
     val gameName = "TrailBlazer"
     println("********************************")
@@ -45,32 +56,103 @@ object startupFunctions {
     println("\t" + "\t" + "    Options")
     println("\t" + "\t" + "     Exit")
     println()
-    println("Please enter one of the above options: ")
   }
-  //login, options, exit
 
+
+  // get user input and compare to list of options and open based on comparison
+  // options include login, options, and exit
+  def titleScreenInputs(): Unit = {
+    val titleInput = userCommands.getInput()
+
+    if (titleInput == "login") {
+      println("Login selected" + "\n")
+      loginFunctions.loginScreen()
+      loginFunctions.checkUsername()
+    } else if (titleInput == "options") {
+      println("Options selected" + "\n")
+    } else if (titleInput == "exit") {
+      println("Exit selected" + "\n" + "\n")
+      sys.exit(0)
+    } else {
+      println("I'm sorry, that is not one of the options above.")
+      startupFunctions.titleScreenInputs()
+    }
+  }
 
 
   // accounts table holds username, emailaddress, and password in sql with the possibility of adding saved state later
 
-
-
-
-
 }
 
+//create function definitions for logging in
+object loginFunctions {
+  //display login screen and ask for username
+  def loginScreen(): Unit = {
+    val stringName = "LOGIN"
+    println("********************************")
+    print("*************")
+    print(s"${BOLD} ${UNDERLINED}$stringName${RESET}")
+    println(" ************")
+    println("********************************")
+    println()
+  }
 
-//create functions for starting game after logging in
+  def checkUsername(): Unit = {
+    val nameInput = userCommands.getUserName()
 
-object loggedIn {
-  def startScreen(): Unit = {
-
+    if (nameInput == "back") {
+      println("\n" +"\n")
+      startupFunctions.runStartUp()
+    } else {
+      println("hello")
+    }
 
   }
 
-  //start new game, continue, exit in gamescreen
 
 }
+
+//create function definitions for options
+object optionsFunctions {
+  def optionsScreen(): Unit = {
+    val stringName = "OPTIONS"
+    println("********************************")
+    print("*********")
+    print(s"${BOLD} ${UNDERLINED}$stringName${RESET}")
+    println(" **********")
+    println("********************************")
+    println()
+    println("\t" + "\t" + "     Login")
+    println("\t" + "\t" + "    Options")
+    println("\t" + "\t" + "     Exit")
+    println()
+  }
+
+
+}
+
+
+//create functions for running the game after logging in
+
+
+// create a list of functions users can implement
+object userCommands {
+
+  //this function is for any user string inputs, returns lowercase string for boolean comparisons
+  def getInput(): String = {
+    val inputString = readLine("Please enter one of the above options: ")
+    val inputToLower = inputString.toLowerCase
+    return inputToLower
+  }
+
+  def getUserName(): String = {
+    val inputString = readLine("Please enter existing username or 'back' to go back: ")
+    val inputToLower = inputString.toLowerCase
+    return inputToLower
+  }
+
+}
+
 
 
 //create function to connect to database
@@ -105,7 +187,7 @@ object Connect_to_DB {
 
   //this function was created for debugging purposes
   def show_Databases(): Unit = {
-    val jdbcDatabase = "myfirst"
+    val jdbcDatabase = "projectzero"
     val url = s"jdbc:mysql://localhost/${jdbcDatabase}"
     val username = "root"
     val password = "Z3r02021!!"
@@ -140,13 +222,44 @@ object Connect_to_DB {
   }
 
 
+  def show_Usernames(): Unit = {
+    val jdbcDatabase = "projectzero"
+    val url = s"jdbc:mysql://localhost/${jdbcDatabase}"
+    val username = "root"
+    val password = "Z3r02021!!"
+
+    try {
+      // make the connection
+      connection = DriverManager.getConnection(url, username, password)
+      println("Connection established......")
+      // create the statement, and run the select query
+      val statement = connection.createStatement()
+      //retrieve the data
+      //get tables from database
+      // val dbSet = statement.executeQuery("Use myfirst")
+      val tableSet = statement.executeQuery("Show Tables")
+      println("List of tables: ")
+      while ( tableSet.next() ) {
+        print(tableSet.getString(1))
+        println()
+      }
+
+      val userSet = statement.executeQuery("SELECT username FROM projectzero.`project0-accounts`")
+      println("List of Usernames: ")
+      while ( userSet.next() ) {
+        print(userSet.getString(1))
+        println()
+      }
+
+
+    } catch {
+      case e: Exception => e.printStackTrace
+    }
+    connection.close()
+  }
+
+
 
 
 }
 
-
-
-// You will want to create a class that takes in values for int id, int mapid, string description.
-class TrailBlazer {
-
-}
